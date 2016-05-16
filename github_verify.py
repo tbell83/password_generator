9c23aad79c55e848aacfc7817712ba983f5e4a30
Thomas Bell <thomasb@aweber.com>
2016-05-15 20:53:15 -0400

    changing the secret

--- github_verify.py
+++ github_verify.py
@@ -2,11 +2,10 @@ import hashlib
 import hmac
 import os
 
-secret = os.environ['SECRET']
+github_secret = bytearray(os.environ['SECRET'], 'UTF-8')
 
 
 def verifyHmacHash(data, signature):
-    github_secret = bytearray('ThisIsTheSecret', 'UTF-8')
     mac = hmac.new(github_secret, msg=data, digestmod=hashlib.sha1)
     calculated_sig = 'sha1=' + mac.hexdigest()
     if calculated_sig == signature:
